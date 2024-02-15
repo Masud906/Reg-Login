364e7f1f0f920e2d4dd20f0644b8a70eb63606a8
masud rana <Masud Rana>
2024-02-15 22:26:22 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtSS7XNaPgUMgbmmof4g0BefG2FJLcWKs",
+  authDomain: "auth-moha-milon-4a09d.firebaseapp.com",
+  projectId: "auth-moha-milon-4a09d",
+  storageBucket: "auth-moha-milon-4a09d.appspot.com",
+  messagingSenderId: "834642707360",
+  appId: "1:834642707360:web:c923f075d893ce9cb81ac5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
